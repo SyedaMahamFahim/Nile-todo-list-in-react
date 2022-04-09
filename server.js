d9aa279747c35c42cc0709f2b127a53854038047
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-04-09 06:46:03 +0500

    added-cors

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const app = require("./app");
 const database=require('./database/configuration')
 const PORT=process.env.PORT || 8000
 
+
+//Insurance to prevent cors error
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+  }
+  next();
+})
+
+
+
+
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`);
