8da14f3f0e1279faa3e9eee3307dffea3fda79d3
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-04-09 23:55:30 +0500

    cors-library-added

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const todoRoute =require('./routes/todoRoutes')
 const errorMiddleware=require('./middleware/errors')
 const cors = require("cors");
 
+
 // Middlewares
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(cors())
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
